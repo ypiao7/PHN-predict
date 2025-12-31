09da18fd25970112c8f23ed8b37aa9e6aedb9fb3
Yongjun Piao <144299692+ypiao7@users.noreply.github.com>
2025-12-31 14:51:36 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -107,7 +107,10 @@ def predict_from_request():
     prediction_proba = model.predict_proba(x_final)[:, 1]  # 取 PHN 阳性的概率 (第二列)
     prediction_class = model.predict(x_final)[0]  # 取预测的类别 (0 或 1)
 
-    return prediction_class, prediction_proba, x_final
+    proba = float(prediction_proba[0])  # 或者 prediction_proba[0].item()
+    pred = int(prediction_class)
+
+    return pred, proba, x_final
 
 # =========================
 # 6) 页面路由（Render 打开也能用）
